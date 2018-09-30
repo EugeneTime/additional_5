7b64d119e5c5b9bd9547a8030e602ff1203cbbf5
EugeneTime <eugenetime@bk.ru>
2018-09-30 14:01:50 +0300

    EugeneTime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+
+  let temp = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][1] && temp[temp.length - 1] === bracketsConfig[j][0]) temp.pop();
+        else if (str[i] === bracketsConfig[j][0]) temp.push(str[i]);
+    }
+  }
+
+  if (temp.length === 0) return true;
+  return false;
+};
